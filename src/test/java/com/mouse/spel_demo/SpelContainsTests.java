08c73275e420c6915526534dd450ecc315d5ceae
gongchangyou <gongchangyou@gmail.com>
2024-11-16 11:30:27 +0800

    contains test

--- src/test/java/com/mouse/spel_demo/SpelContainsTests.java
+++ src/test/java/com/mouse/spel_demo/SpelContainsTests.java
@@ -42,11 +42,66 @@ class SpelContainsTests {
         }
     }
 
+    public static boolean like(String s, String p) {
+        final boolean isMatched = FilenameUtils.wildcardMatch(s, p);
+        return isMatched;
+    }
+
+    @Test
+    void testContains100 () throws NoSuchMethodException {
+        StopWatch sw = new StopWatch();
+        for (int i=0;i<100;i++) {
+            sw.start();
+            testContains();
+            sw.stop();
+        }
+        log.info(sw.prettyPrint());
+
+    }
+
+    /**
+     * 执行时间0.2ms
+     * @throws NoSuchMethodException
+     */
+    @Test
+    void testContains() throws NoSuchMethodException {
+        // Create a SpEL expression parser
+        ExpressionParser parser = new SpelExpressionParser();
+
+        // Enumerations in SpEL are accessed using their fully qualified names
+        val enumExpression = parser.parseExpression("{'BOND', 'IRS'}.contains(#instrumentClass) and #like(#appId, 'peak*') and #side=='BUY' and #dv01==1");
+        StandardEvaluationContext context = new StandardEvaluationContext();
+        context.setVariable("instrumentClass", "BOND");
+        context.setVariable("appId", "peak_algo");
+        context.setVariable("side", "BUY");
+        context.setVariable("dv01", 1);
+
+        context.setVariable("like", SpelContainsTests.class.getDeclaredMethod("like", String.class, String.class));
+
+        // Evaluate the SpEL expression
+        boolean containsValue = enumExpression.getValue(context, Boolean.class);
+
+        // Check if the enum contains the specified value
+        if (containsValue) {
+            System.out.println("contains.");
+        } else {
+            System.out.println("does not contain.");
+        }
+    }
+
     @Test
     void testEqual() {
         // Create a SpEL expression parser
         ExpressionParser parser = new SpelExpressionParser();
+        val enumExpression11 = parser.parseExpression("false ? 3 : 4");
+        int b = 2 ^ 3;
+        int result = parser.parseExpression("5 ^ 3").getValue(Integer.class);
+        //下面这个会抛异常
+        //        val enumExpression = parser.parseExpression("T(com.mouse.MyEnum).valueOf('BUYxx') != null");
 
+        // Evaluate the SpEL expression
+        int a = enumExpression11.getValue(Integer.class);
+        System.out.println(a);
         // Enumerations in SpEL are accessed using their fully qualified names
         // Replace 'com.example.MyEnum' with the actual package and enum name
         StopWatch sw = new StopWatch();
